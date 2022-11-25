7156de8836dfb1e5ed21d106d57d007ef7a0a9ab
Sondre Agledahl <sondre@sondre.io>
2022-11-25 09:14:47 +0000

    Player movement

--- raycast.js
+++ raycast.js
@@ -35,7 +35,64 @@ class Map {
     }
 }
 
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2.0;
+        this.y = WINDOW_HEIGHT / 2.0;
+        this.radius = 3;
+        this.turnDirection = 0;
+        this.walkDirection = 0;
+        this.rotationAngle = Math.PI / 2.0;
+        this.moveSpeed = 2.0;
+        this.rotationSpeed = 2 * (Math.PI / 180);
+    }
+
+    update() {
+        const moveStep = this.walkDirection * this.moveSpeed;
+        
+        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+        this.x += moveStep * Math.cos(this.rotationAngle);
+        this.y += moveStep * Math.sin(this.rotationAngle);
+    }
+
+    render() {
+        noStroke();
+        fill("red");
+        circle(this.x, this.y, this.radius);
+        stroke("red");
+        line(this.x, this.y,
+             this.x + 30 * Math.cos(this.rotationAngle), this.y + 30 * Math.sin(this.rotationAngle)
+        );
+    }
+}
+
+
 var grid = new Map();
+var player = new Player();
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = 1;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = -1;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 1;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = -1;
+    }
+}
+
+function keyReleased() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = 0;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = 0;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 0;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = 0;
+    }
+}
 
 function setup() {
     createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -44,10 +101,12 @@ function setup() {
 
 function update() {
     // TODO: update all game objects before we render the next frame
+    player.update();
 }
 
 function draw() {
     update();
 
     grid.render();
+    player.render();
 }
