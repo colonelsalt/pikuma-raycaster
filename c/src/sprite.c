732763563daa2eafa38adc02b14255751c3dcbf0
Sondre <sondre@sondre.io>
2023-01-03 10:56:29 +0000

    Drawing sprites

--- c/src/sprite.c
+++ c/src/sprite.c
@@ -6,10 +6,12 @@
 #include "defs.h"
 #include "utils.h"
 
-#define NUM_SPRITES 2
+#define NUM_SPRITES 3
 
 static sprite_t sprites[NUM_SPRITES] = {
-    { .x = 640, .y = 630, .texture = 9 } // barrel
+    { .x = 640, .y = 630, .texture = 9 }, // barrel
+    { .x = 250, .y = 600, .texture = 11 }, // table
+    { .x = 300, .y = 400, .texture = 12 } // guard
 };
 
 void renderMapSprites()
@@ -38,7 +40,8 @@ void renderSprites()
             angle += TWO_PI;
         angle = fabs(angle);
 
-        if (angle < FOV_ANGLE / 2)
+        const float EPSILON = 0.2f;
+        if (angle < (FOV_ANGLE / 2) + EPSILON)
         {
             sprites[i].visible = true;
             sprites[i].angle = angle;
@@ -55,6 +58,46 @@ void renderSprites()
     // Render visible sprites
     for (int i = 0; i < numVisibleSprites; i++)
     {
+        sprite_t sprite = visibleSprites[i];
 
+        float spriteHeight = (TILE_SIZE / sprite.distance) * DIST_PROJ_PLANE;
+        float spriteWidth = spriteHeight;
+
+        // Sprite top y
+        float spriteTopY = (WINDOW_HEIGHT / 2) - (spriteHeight / 2);
+        if (spriteTopY < 0)
+            spriteTopY = 0;
+        
+        float spriteBottomY = (WINDOW_HEIGHT / 2) + (spriteHeight / 2);
+        if (spriteBottomY > WINDOW_HEIGHT)
+            spriteBottomY = WINDOW_HEIGHT;
+
+        float spriteAngle = atan2(sprite.y - player.y, sprite.x - player.x) - player.rotationAngle;
+        float spriteScreenPosX = tan(spriteAngle) * DIST_PROJ_PLANE;
+
+        float spriteLeftX = (WINDOW_WIDTH / 2) + spriteScreenPosX - (spriteWidth / 2);
+        float spriteRightX = spriteLeftX + spriteWidth;
+
+        int texWidth = upng_get_width(textures[sprite.texture]);
+        int texHeight = upng_get_height(textures[sprite.texture]);
+
+        for (int y = spriteTopY; y < spriteBottomY; y++)
+        {
+            int distFromTop = y + (spriteHeight / 2) - (WINDOW_HEIGHT / 2);
+            int texOffsetY = distFromTop * (texHeight / spriteHeight);
+            for (int x = spriteLeftX; x < spriteRightX; x++)
+            {
+                float texelWidth = texWidth / spriteWidth;
+                int texOffsetX = (x - spriteLeftX) * texelWidth;
+                if (x > 0 && x < WINDOW_WIDTH && y > 0 && y < WINDOW_HEIGHT)
+                {
+                    color_t* texBuffer = (color_t*)upng_get_buffer(textures[sprite.texture]);
+                    color_t texelColor = texBuffer[(texWidth * texOffsetY) + texOffsetX];
+                    
+                    if (texelColor != 0xFFFF00FF)
+                        setPixel(x, y, texelColor);
+                }
+            }
+        }
     }
 }
